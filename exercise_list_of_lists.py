9a259f4317dd6f58f9405164b39242c999c17dec
facundoestradaa <es.facundo.08@gmail.com>
2026-04-07 11:43:15 -0300

    Update exercise_list_of_lists.py

--- exercise_list_of_lists.py
+++ exercise_list_of_lists.py
@@ -13,4 +13,17 @@ def list_of_lists(lista_de_listas):
     Returns:
         La lista de listas modificada según las reglas
     """
-    pass  # Reemplazar con tu implementación
+
+    lista1 = lista_de_listas[0]
+    lista2 = lista_de_listas[1]
+    lista3 = lista_de_listas[2]
+
+
+    lista_de_listas[0] = lista1[0:2]
+    lista_de_listas[1] = lista2[1:4]
+    lista_de_listas[2] = lista3[-2:]
+    return lista_de_listas
+
+
+
+
