a2c55e0857d5e4d45a2bee7ec1e04a8e9e0f4842
facundoestradaa <es.facundo.08@gmail.com>
2026-04-07 11:41:21 -0300

    Update exercise_check_lists.py

--- exercise_check_lists.py
+++ exercise_check_lists.py
@@ -12,4 +12,14 @@ def check_lists(lista1, lista2):
     Returns:
         True si ambas listas tienen el mismo tercer elemento, False en caso contrario
     """
-    pass  # Reemplazar con tu implementación
+
+    if len(lista1) >=3 and  len(lista2) >=3:
+        return lista1[2] == lista2[2]
+
+    else:
+        return False
+
+
+
+
+
